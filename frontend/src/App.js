555bf0a704b8e234a2dcd269a7db91f6b36e940f
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-08-12 15:03:18 +0200

    Implemented redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,21 @@ import RegisterPage from "./pages/RegisterPage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/register" exact component={RegisterPage} />
-        <Route path="/login" exact component={LoginPage} />
-      </Switch>
-      <Footer />
+      <Provider store={store}>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/register" exact component={RegisterPage} />
+          <Route path="/login" exact component={LoginPage} />
+        </Switch>
+        <Footer />
+      </Provider>
     </Router>
   );
 };
