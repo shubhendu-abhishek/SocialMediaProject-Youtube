2943639c3241447af002c54b0c7e7971dfb32cd6
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-08-16 14:13:06 +0200

    Made basic function and added properly login user function

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { loginUser } from "../actions/auth.actions";
+import { connect } from "react-redux";
 
-const LoginPage = () => {
+const LoginPage = ({ loginUser }) => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = userData;
+
+  const onChange = (e) =>
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <div>
+        <label>email</label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => onChange(e)}
+        />
+      </div>
+      <br />
+      <div>
+        <label>password</label>
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => onChange(e)}
+        />
+      </div>
+
+      <br />
+      <button onClick={() => loginUser(userData)}>Submit form</button>
     </div>
   );
 };
 
-export default LoginPage;
+export default connect(null, { loginUser })(LoginPage);
