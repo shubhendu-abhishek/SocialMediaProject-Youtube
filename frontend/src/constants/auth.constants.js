c8e8c50706ad4087c8272ea4692c4af024386850
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-08-16 14:15:45 +0200

    Added authentication constants

---
+++ frontend/src/constants/auth.constants.js
@@ -0,0 +1,5 @@
+export const AUTH_FORM_SUCCESS = "AUTH_FORM_SUCCESS";
+export const AUTH_FORM_FAIL = "AUTH_FORM_FAIL";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const USER_IS_LOADED = "USER_IS_LOADED";
+export const LOG_OUT = "LOG_OUT";
