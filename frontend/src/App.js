96f2f8983f5b69b07ec578a7fd28283f9f591346
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-08-16 14:12:24 +0200

    Added auth routes and user loaded function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -7,8 +7,17 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
+import setAuthenticationToken from "./middleware/setAuthenticationToken";
+import { userLoaded } from "./actions/auth.actions";
+
+if (localStorage.getItem("token")) {
+  setAuthenticationToken(localStorage.getItem("token"));
+}
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(userLoaded());
+  }, []);
   return (
     <Router>
       <Provider store={store}>
