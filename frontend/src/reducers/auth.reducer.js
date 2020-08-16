9ceafb0d1509f17fac897f0683bc7aa0c2f32f01
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-08-16 14:15:08 +0200

    Added authentication cases

--- frontend/src/reducers/auth.reducer.js
+++ frontend/src/reducers/auth.reducer.js
@@ -1,8 +1,64 @@
-const initialState = {};
+import {
+  AUTH_FORM_SUCCESS,
+  AUTH_FORM_FAIL,
+  AUTH_ERROR,
+  USER_IS_LOADED,
+  LOG_OUT,
+} from "../constants/auth.constants";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  users: {},
+  user: {},
+  errors: {},
+  isLoggedIn: false,
+  isAllowedToChangePassword: false,
+  isPasswordChanged: false,
+  isLoading: false,
+};
 
 const auth = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
+    case AUTH_FORM_SUCCESS:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        ...payload,
+        isLoggedIn: true,
+        isAllowedToChangePassword: false,
+        isPasswordChanged: false,
+        isLoading: false,
+        errors: {},
+      };
+    case AUTH_FORM_FAIL:
+    case AUTH_ERROR:
+    case LOG_OUT:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        ...payload,
+        errors: payload,
+        users: {},
+        user: {},
+        isLoggedIn: false,
+        isAllowedToChangePassword: false,
+        isPasswordChanged: false,
+        isLoading: false,
+      };
+    case USER_IS_LOADED:
+      localStorage.getItem("token");
+      return {
+        ...state,
+        ...payload,
+        users: {},
+        user: payload,
+        errors: {},
+        isLoggedIn: true,
+        isAllowedToChangePassword: false,
+        isPasswordChanged: false,
+        isLoading: false,
+      };
     default:
       return state;
   }
