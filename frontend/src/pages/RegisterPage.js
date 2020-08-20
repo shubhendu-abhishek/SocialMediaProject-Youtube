41f82ae819575534a85e374a32d9a05150998c53
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-08-20 10:26:40 +0200

    Added Register Page styles

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { registerUser } from "../actions/auth.actions";
 import { connect } from "react-redux";
 
-const RegisterPage = ({ registerUser }) => {
+const RegisterPage = ({ registerUser, error }) => {
+  const [hasPasswordShowed, setShowPassword] = useState(false);
+
   const [userData, setUserData] = useState({
     name: "",
     lastName: "",
@@ -17,61 +19,108 @@ const RegisterPage = ({ registerUser }) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
 
   return (
-    <div>
-      <div>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => onChange(e)}
-        />
-      </div>
-      <br />
-      <div>
-        <label>Last name</label>
-        <input
-          type="text"
-          name="lastName"
-          value={lastName}
-          onChange={(e) => onChange(e)}
-        />
-      </div>
-      <br />
-      <div>
-        <label>user name</label>
-        <input
-          type="text"
-          name="userName"
-          value={userName}
-          onChange={(e) => onChange(e)}
-        />
-      </div>
-      <br />
-      <div>
-        <label>email</label>
-        <input
-          type="text"
-          name="email"
-          value={email}
-          onChange={(e) => onChange(e)}
-        />
-      </div>
-      <br />
-      <div>
-        <label>password</label>
-        <input
-          type="text"
-          name="password"
-          value={password}
-          onChange={(e) => onChange(e)}
-        />
-      </div>
+    <main className="register-page-wrapper">
+      <form className="register-section">
+        <div className="inputs-wrapper">
+          <header className="register-header-wrapper">
+            <p className="font__p p__size register-header">
+              <i className="fas fa-users users-icon app_color_font"></i>
+              Sign Up
+            </p>
+          </header>
 
-      <br />
-      <button onClick={() => registerUser(userData)}>Submit form</button>
-    </div>
+          <div className="label-wrapper">
+            <label className="label__register p__size">Name</label>
+          </div>
+
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+          />
+
+          <div className="label-wrapper">
+            <label className="label__register p__size">Last Name</label>
+          </div>
+          <input
+            type="text"
+            value={lastName}
+            name="lastName"
+            onChange={(e) => onChange(e)}
+          />
+
+          <div className="label-wrapper">
+            <label className="label__register p__size">Username</label>
+          </div>
+          <input
+            type="text"
+            name="userName"
+            value={userName}
+            onChange={(e) => onChange(e)}
+          />
+
+          <div className="label-wrapper">
+            <label className="label__register p__size">E-mail</label>
+          </div>
+          <input
+            name="email"
+            value={email}
+            type="email"
+            onChange={(e) => onChange(e)}
+          />
+
+          <div className="label-wrapper">
+            <label className="label__register p__size">Password</label>
+          </div>
+
+          <input
+            name="password"
+            type={hasPasswordShowed ? "text" : "password"}
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+
+          <i
+            onClick={() => setShowPassword(!hasPasswordShowed)}
+            className={hasPasswordShowed ? "fas fa-eye" : "fas fa-eye-slash"}
+          ></i>
+
+          <div className="label-wrapper">
+            <p className="p__size font__p password__info">
+              <i className="fas fa-user-check app_color_font"></i> Password must
+              have at least 6 letters
+            </p>
+          </div>
+
+          {error && (error !== null || error !== "" || error !== {}) && (
+            <div>
+              <p
+                className="font__bold font__p p__size"
+                style={{
+                  color: "#fb2f2f",
+                  textAlign: "center",
+                }}
+              >
+                Something went wrong...
+              </p>
+            </div>
+          )}
+
+          <div
+            className="button-wrapper app_color_background"
+            onClick={() => registerUser(userData)}
+          >
+            <p className="button-letter">Sign Up</p>
+          </div>
+        </div>
+      </form>
+    </main>
   );
 };
 
-export default connect(null, { registerUser })(RegisterPage);
+const mapStateToProps = (state) => ({
+  error: state.auth.errors,
+});
+
+export default connect(mapStateToProps, { registerUser })(RegisterPage);
