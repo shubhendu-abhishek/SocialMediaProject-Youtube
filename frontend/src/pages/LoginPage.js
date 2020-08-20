ad1908f9a0fe459f07c6d2c25a6a91e27c3c0546
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-08-20 10:26:13 +0200

    Added Login Page styles?

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { loginUser } from "../actions/auth.actions";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-const LoginPage = ({ loginUser }) => {
+const LoginPage = ({ loginUser, error }) => {
   const [userData, setUserData] = useState({
     email: "",
     password: "",
@@ -14,31 +15,69 @@ const LoginPage = ({ loginUser }) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
 
   return (
-    <div>
-      <div>
-        <label>email</label>
-        <input
-          type="text"
-          name="email"
-          value={email}
-          onChange={(e) => onChange(e)}
-        />
-      </div>
-      <br />
-      <div>
-        <label>password</label>
-        <input
-          type="text"
-          name="password"
-          value={password}
-          onChange={(e) => onChange(e)}
-        />
-      </div>
+    <main className="register-page-wrapper">
+      <form className="register-section">
+        <div className="inputs-wrapper">
+          <header className="register-header-wrapper">
+            <p className="font__p p__size register-header">
+              <i className="fas fa-users users-icon app_color_font"></i>
+              Log in
+            </p>
+          </header>
 
-      <br />
-      <button onClick={() => loginUser(userData)}>Submit form</button>
-    </div>
+          <div className="label-wrapper">
+            <label className="label__register p__size">E-mail</label>
+          </div>
+          <input
+            name="email"
+            value={email}
+            type="email"
+            onChange={(e) => onChange(e)}
+          />
+
+          <div className="label-wrapper">
+            <label className="label__register p__size">Password</label>
+          </div>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+
+          <div className="label-wrapper">
+            <Link to="/register">
+              <p className="p__size font__p password__info">Sign up</p>
+            </Link>
+          </div>
+          {error && (error !== null || error !== "" || error !== {}) && (
+            <div>
+              <p
+                className="font__bold font__p p__size"
+                style={{
+                  color: "#fb2f2f",
+                  textAlign: "center",
+                }}
+              >
+                Wrong e-mail or password
+              </p>
+            </div>
+          )}
+
+          <div
+            className="button-wrapper app_color_background"
+            onClick={(e) => loginUser(userData)}
+          >
+            <p className="button-letter">Log in</p>
+          </div>
+        </div>
+      </form>
+    </main>
   );
 };
 
-export default connect(null, { loginUser })(LoginPage);
+const mapStateToProps = (state) => ({
+  error: state.auth.errors,
+});
+
+export default connect(mapStateToProps, { loginUser })(LoginPage);
