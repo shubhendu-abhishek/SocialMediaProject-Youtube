b5c046f3b78dc6c2bbe75fb496ac6dc1038960f4
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-08-26 18:09:35 +0200

    Moved search by username from users constants to auth constants

--- frontend/src/constants/users.constants.js
+++ frontend/src/constants/users.constants.js
@@ -5,4 +5,3 @@ export const USER_ERROR = "USER_ERROR";
 export const GET_POST_BY_USER_ID = "GET_POST_BY_USER_ID";
 export const GET_USER_POSTS = "GET_USER_POSTS";
 export const GET_USER_BY_ID = "GET_USER_BY_ID";
-export const SEARCH_BY_USERNAME = "SEARCH_BY_USERNAME";
